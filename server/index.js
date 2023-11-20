024e8221c343bbfeab583d5e1e2b51e4c0272d2f
sinvan <muhammedsinvan3@gmail.com>
2023-11-20 14:54:06 +0530

    added /api in entry endpoind

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(express.json({limit:'25mb'}));
 app.use(express.urlencoded({limit:'25mb',extended:true}))
 app.use(cors())
 
-app.use('/',userrouter);
-app.use('/admin',adminrouter)
+app.use('/api/',userrouter);
+app.use('/api/admin',adminrouter)
 
 
 app.listen(8000, ()=>{
