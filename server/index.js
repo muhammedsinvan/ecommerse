2f60da74e22890fcced17be4e9b5df7611c20f90
sinvan <muhammedsinvan3@gmail.com>
2024-02-04 22:34:31 +0530

    heroku added data deleted

--- server/index.js
+++ server/index.js
@@ -5,7 +5,6 @@ import { connectDB } from './connection/database.js';
 import userrouter from './routes/user.js';
 import adminrouter from './routes/admin.js';
 import cors from 'cors';
-import path from 'path';
 
 connectDB();
 
@@ -15,16 +14,6 @@ app.use(express.json({limit:'25mb'}));
 app.use(express.urlencoded({limit:'25mb',extended:true}))
 app.use(cors())
 
-if (process.env.NODE_ENV === 'production'){
-// Serve static files from the React app
-const currentPath = new URL('.', import.meta.url).pathname;
-app.use(express.static(path.join(currentPath, '../cliend/build')));
-
-// Handle React routing, return all requests to React app
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../cliend/build', 'index.html'));
-});
-}
 
 app.use('/api/',userrouter);
 app.use('/api/admin',adminrouter)
