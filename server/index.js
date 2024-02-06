333db9d3951bd792cd3e624598b6b6c5849fe627
sinvan <muhammedsinvan3@gmail.com>
2024-02-06 12:18:19 +0530

    vercel.json file index.js cors updated

--- server/index.js
+++ server/index.js
@@ -12,7 +12,13 @@ const app = express();
 
 app.use(express.json({limit:'25mb'})); 
 app.use(express.urlencoded({limit:'25mb',extended:true}))
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://ecommerse-phi.vercel.app/"],
+        methods: ["POST", "GET"],
+        credentials: true
+    }
+));
 
 
 app.use('/api/',userrouter);
