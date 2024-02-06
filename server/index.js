a922580693a3bc0ba59a78e555c7fa206475fdc9
sinvan <muhammedsinvan3@gmail.com>
2024-02-06 13:20:13 +0530

    cors bug fixe

--- server/index.js
+++ server/index.js
@@ -12,13 +12,12 @@ const app = express();
 
 app.use(express.json({limit:'25mb'})); 
 app.use(express.urlencoded({limit:'25mb',extended:true}))
-app.use(cors(
-    {
-        origin: ["https://ecommerse-phi.vercel.app"],
-        methods: ["POST", "GET"],
-        credentials: true
-    }
-));
+
+app.use(cors());
+
+app.use(cors({
+  origin: 'https://ecommerse-phi.vercel.app'
+}));
 
 
 app.use('/api/',userrouter);
